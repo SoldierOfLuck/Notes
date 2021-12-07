0469ef84abdf3fc89ea6bc9f37f2d2b56e0bce8b
Artur Lukmanov <soldierofluck@gmail.com>
2021-12-07 13:57:06 +0300

    По заданиям 8 и 9 сделал боковое меню и диалоговое окно при выходе из приложения с Toast "Приложение закрыто"

--- app/src/main/java/ru/lukmanov/notes/MainActivity.java
+++ app/src/main/java/ru/lukmanov/notes/MainActivity.java
@@ -1,8 +1,16 @@
 package ru.lukmanov.notes;
 
+import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager()
                         .beginTransaction()
                         .add(R.id.fragment_container, new AppDataFragment()).commit();
+
             }
 
         });
@@ -49,5 +58,65 @@ public class MainActivity extends AppCompatActivity {
     }
     private boolean isLandscape() {
         return getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.side_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_create:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .add(R.id.fragment_container, new NotesFragment()).commit();
+                return true;
+            case R.id.menu_settings:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .add(R.id.fragment_container, new SettingsFragment()).commit();
+                return true;
+            case R.id.menu_about:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .add(R.id.fragment_container, new AppDataFragment()).commit();
+                return true;
+
+            case R.id.action_exit:
+                showAlertDialogWithCustomView();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+        showAlertDialogWithCustomView();
+    }
+
+    private void showAlertDialogWithCustomView() {
+        final View customView = getLayoutInflater().inflate(R.layout.custom_alert_dialog, null);
+        new AlertDialog.Builder(this)
+                .setView(customView)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Приложение закрыто",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Отмена",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
     }
 }
